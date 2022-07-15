4d4a1446286afcf0712d4cb3220ec4d0c43cce7c
TristanVarewijck <tristanconnect4@outlook.com>
2022-07-15 23:16:40 +0200

    dataset

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
 
+const data = require("../data.json");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index");
+router.get("/:id", function (req, res, next) {
+  let teamMember = req.params.id;
+
+  // filter correct object on params..
+  let filteredMember = data.filter((i) => teamMember.includes(i.id));
+
+  // filtered correct user
+  let myData = filteredMember[0].email;
+
+  res.render("index", { data: myData });
 });
 
 module.exports = router;
