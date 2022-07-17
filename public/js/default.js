404e2675dfabaee24c3fe4750feda590509dcfe8
TristanVarewijck <tristanconnect4@outlook.com>
2022-07-17 21:44:29 +0200

    copie to clipboard

--- public/js/default.js
+++ public/js/default.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+// copy to clipboard
+
+const shareButton = document.querySelector(".button-group .share");
+console.log(shareButton);
+
+shareButton.addEventListener("click", (e) => {
+  let value = e.currentTarget.name;
+  navigator.clipboard.writeText(value);
+
+  console.log("Copied Text: " + value);
+});
